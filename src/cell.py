22957966b395b4e107a6fb40fb01da2be06026a9
KoloskovaMariia <koloskovamariia@gmail.com>
2022-06-16 17:13:43 +0500

    doc strings for ui

--- src/cell.py
+++ src/cell.py
@@ -12,6 +12,7 @@ class Cell(QLabel):
     clicked = QtCore.pyqtSignal()
 
     def mousePressEvent(self, q_mouse_event):
+        """extension for the label to track clicks on it"""
         self.clicked.emit()
 
     def __init__(self, text, click_label):
@@ -32,10 +33,12 @@ class Cell(QLabel):
         self.clicked.connect(self.click_label)
 
     def click_label(self) -> None:
+        """increase click count and change label color"""
         self.increase_click_count()
         self.set_color()
 
     def clean_label(self) -> None:
+        """remove label color"""
         self.color = Color(255, 255, 255)
         self.setStyleSheet(
             f"background-color:rgb"
@@ -44,9 +47,11 @@ class Cell(QLabel):
             f"\"MS Shell Dlg 2\";color:rgb(67, 65, 49);")
 
     def increase_click_count(self) -> None:
+        """increase click count"""
         self.ClickLabel.setText(str(int(self.ClickLabel.text()) + 1))
 
     def set_color(self) -> None:
+        """set color to label"""
         if self.color == Color(255, 255, 255):
             color = self.random_choose_color()
             if Utils.in_collection(color):
@@ -63,6 +68,7 @@ class Cell(QLabel):
 
     @staticmethod
     def random_choose_color() -> Color:
+        """generate random color for label"""
         red = randint(0, 254)
         green = randint(0, 254)
         blue = randint(0, 254)
